6d16cd1256d375d4b080197ae0212442c791f0f5
Rittick Ghosh <55430271+RittikGhosh@users.noreply.github.com>
2022-12-20 00:50:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+//your code here
+let input = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+
+
+input.addEventListener("keyup", updatecounter);
+function updatecounter() {
+    let str = input.value;
+    let c = str.length;
+    count.innerText = c;
+}
